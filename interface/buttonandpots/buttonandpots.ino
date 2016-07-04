7816ce4ed106b0d09625975addb3754a1adeea19
lachose1 <hugolaliberte@gmail.com>
2016-07-04 12:51:52 -0400

    json arduino code

--- interface/buttonandpots/buttonandpots.ino
+++ interface/buttonandpots/buttonandpots.ino
@@ -1,15 +1,13 @@
-#include <ArduinoJson.h>
-
-StaticJsonBuffer<200> jsonBuffer;
+#include <string.h>
 
 int btnPins[1] = {2};
 int btns[1];
-int potsPins[1] = {A0};
+int potsPins[3] = {A0, A1, A2};
 int pots[1];
 int stations = 0;
 
 void setup() {
-  stations = sizeof(btnPins); //Detect how many stations are plugged
+  stations = sizeof(btnPins) - 1; //Detect how many stations are plugged
   for(byte i = 0; i < stations; i++) // declare pushbuttons as inputs
     pinMode(btnPins[i], INPUT);
 
@@ -17,20 +15,37 @@ void setup() {
 }
 
 void loop(){
-//  JsonObject& root = jsonBuffer.createObject();
-  
-  for(byte i = 0; i < stations; i++)
+  String json = "{";
+  for(byte i = 0; i < stations * 3; i++)
     pots[i] = analogRead(potsPins[i]); // Read pots
   
   for(byte i = 0; i < stations; i++)
     btns[i] = digitalRead(btnPins[i]);  // Read input value
     
-
-  Serial.println(pots[0]);
-  if (btns[0] == HIGH) {         // check if the input is HIGH (button released)
-    Serial.println("0");
-  } else {
-    Serial.println("1");
+  for(byte i = 0; i < stations; i++) {
+     json += "'station";
+     json += i;
+     json += "': {'btn': ";
+     json += !btns[0];
+     json += ", ";
+     for(byte j = 0; j < 3; j++) {
+          json += "'pot";
+          json += j;
+          json += "': ";
+          json += pots[j];
+     }
+     json += "}";
   }
+  json += "}";
+  Serial.println(json);
+//  for(byte i = 0; i < stations; i++)
+//    json += "'station1' : {'button1': " + "1" + "}";
+//  Serial.println(btns[0]);
+//  Serial.println(pots[0]);
+//  if (btns[0] == HIGH) {         // check if the input is HIGH (button released)
+//    Serial.println("0");
+//  } else {
+//    Serial.println("1");
+//  }
   delay(50); //Delay to avoid flicker
 }
